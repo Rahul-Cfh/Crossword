72866ac40e658855767f962bdec37f47470bb5d3
Rahul-Cfh <rahul.cfh@gmail.com>
2026-05-12 15:43:17 +0530

    Updated gridData.ts

--- src/gridData.ts
+++ src/gridData.ts
@@ -78,7 +78,7 @@ export const WORDS: Word[] = [
     direction: 'across',
     clueNumber: 9,
     startRow: 10,
-    startCol: 4,
+    startCol: 3,
     answer: 'STATEMENTS',
     clue: 'Where the real story shows up',
   },
